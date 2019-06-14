fe0c58157bbb4dab231ff307d6fa698942ef54b2
pierre.costa <pierre.costa@live.fr>
2019-06-14 15:54:13 +0200

    change server config

--- Server.js
+++ Server.js
@@ -3,6 +3,13 @@ var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
 
+const { Gitlab } = require('gitlab');
+const api = new Gitlab({
+  url: 'http://137.74.197.63:8084',
+  token: 'abcdefghij123456',
+  rejectUnauthorized: false
+})
+
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
@@ -30,21 +37,6 @@ app.listen(3000,function(){
   console.log("Live at Port 3000");
 });
 
-
-import { Gitlab } from 'gitlab';
-
-// ES5, assuming native or polyfilled Promise is available
-const { Gitlab } = require('gitlab');
-
-const api = new Gitlab({
-  url: 'http://137.74.197.63:8083',
-  token: 'abcdefghij123456',
-  rejectUnauthorized: false
-})
-
-// Listing users
-//let users = await api.Users.all();
- 
 // Or using Promise-Then notation
 api.Projects.all().then(projects => {
   console.log(projects);
